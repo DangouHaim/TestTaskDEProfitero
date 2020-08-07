5d8edad216c432543c8a19c8fb002f061f181eaf
Dangou Haim <dangouhaim@mail.ru>
2020-08-08 00:26:29 +0300

    Images fix

--- main.rb
+++ main.rb
@@ -29,17 +29,17 @@ class Main
 
         @repository = NetworkRepository.new(source, true)
 
-        call = Proc.new { init() }
+        call = Proc.new { init }
 
-        p 'Init elapsed time : ' + elapsed(call)[0].to_s()
+        p 'Init elapsed time : ' + elapsed(call)[0].to_s
 
         puts "<< #{self.class} : #{__method__}"
     end
 
-    def init()
+    def init
         puts ">> #{self.class} : #{__method__}"
 
-        categoryPage = ''
+        categoryPage = '?categorias=barritas-para-perros'
         pageButton = '//div[@class="pro_outer_box"]/div[contains(@class, "product-desc")]/a[1]/@href'
         pagination = "p"
 
@@ -51,7 +51,7 @@ class Main
     end
 
     public
-    def parse()
+    def parse
         puts ">> #{self.class} : #{__method__}"
 
         # Thread safe array
@@ -61,8 +61,8 @@ class Main
         # Using thread pool to optimize accessing to threads
         @pool = Thread.pool(8)
         
-        if(@repository.any?())
-            @repository.all().each() do |page|
+        if(@repository.any?)
+            @repository.all.each do |page|
 
                 @pool.process do
                     call = Proc.new do
@@ -74,27 +74,34 @@ class Main
     
                     res = elapsed(call)
     
-                    puts 'Get elapsed time : ' + res[0].to_s()
-                    elapsed_times << res[0].to_s()
+                    puts 'Get elapsed time : ' + res[0].to_s
+                    elapsed_times << res[0].to_s
 
                     # Get product title
-                    product = res[1][0][0].children.text.strip()
+                    product = res[1][0][0].children.text.strip
 
                     # Get product photo
-                    image = res[1][2][0].children.text.strip()
+                    image = res[1][2][0].children.text.strip
+                    
+                    # Preparing image id
+                    image_id = image.gsub(/[^0-9]/, '')
+                    image_id = image_id.to_i
+                    image_origin_id = image_id
 
-                    res[1][1].each_with_index() do |item, i|
+                    res[1][1].each_with_index do |item, i|
 
                         result = []
 
+                        image_id += i
+
                         # Get variation title
-                        title = item.xpath('//span[@class="radio_label"]').children[i].text.strip()
+                        title = item.xpath('//span[@class="radio_label"]').children[i].text.strip
                         # Get variation price
-                        price = item.xpath('//span[@class="price_comb"]').children[i].text.strip()
+                        price = item.xpath('//span[@class="price_comb"]').children[i].text.strip
 
                         result << product + " - " + title
                         result << price
-                        result << image
+                        result << image.sub(image_origin_id.to_s, image_id.to_s)
 
                         results << result
 
@@ -105,7 +112,7 @@ class Main
             end
         end
 
-        @pool.shutdown()
+        @pool.shutdown
 
         self.on_data_ready.invoke(self, results)
 
@@ -114,14 +121,14 @@ class Main
 end
 
 def elapsed(method)
-    time = Time.now()
+    time = Time.now
 
-    result = method.call()
+    result = method.call
 
-    return [ Time.now() - time, result ]
+    return [ Time.now - time, result ]
 end
 
-def main()
+def main
     main = Main.new
     handler = MainHandler.new
 
@@ -129,13 +136,13 @@ def main()
     main.on_data_ready.bind(handler.method(:on_data_handler))
 
     # Process data
-    call = Proc.new { main.parse() }
+    call = Proc.new { main.parse }
 
-    elapsed = "Parse elapsed time: " + elapsed(call)[0].to_s()
+    elapsed = "Parse elapsed time: " + elapsed(call)[0].to_s
 
     p elapsed
 end
 
-call = Proc.new { main() }
+call = Proc.new { main }
 
-puts "Total elapsed time: " + elapsed(call)[0].to_s()
+puts "Total elapsed time: " + elapsed(call)[0].to_s
